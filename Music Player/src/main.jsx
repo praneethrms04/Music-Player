072971956dd71898db392b9331841ca939845c88
praneethrms04 <praneethrms04@gmail.com>
2023-04-07 17:23:18 +0530

    project setup

---
+++ Music Player/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.ss.dev/resource/api",
+  cache: new InMemoryCache(),
+});
+
+// https://api.ss.dev/resource/api
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
